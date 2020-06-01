07049fc04b0f925387356dd60fedaa98ade64147
Seth Skinner <sethwskinner@gmail.com>
2020-05-31 23:40:08 -0500

    building the basiv functions for elements in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,24 @@
 var highscore
 var timer
+var startButton = document.getElementById('start-btn')
+var questionContainerElement = document.getElementById('')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.leg('started'
+    startButton.classList.add('hide')
+    questionContainerElement.classList.remove('hide')
+    setNextQuestion()
+}
+
+function setNextQuestion() {
+
+
+}
+
+function selectAnswer() {
+
+}
+
+var questions =
