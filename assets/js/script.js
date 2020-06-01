9757d917dffed7bcea7453eee0f4cd947b5c8d04
Seth Skinner <sethwskinner@gmail.com>
2020-05-31 23:54:12 -0500

    added function to show questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 var highscore
 var timer
 var startButton = document.getElementById('start-btn')
-var questionContainerElement = document.getElementById('')
+var questionContainerElement = document.getElementById('queation-container')
+var questionElement = document.getElementById('question')
+var answerButtonsElement = document.getElementById('answer-btn')
 
 startButton.addEventListener('click', startGame)
 
 function startGame() {
-    console.leg('started'
-    startButton.classList.add('hide')
-    questionContainerElement.classList.remove('hide')
-    setNextQuestion()
+    console.leg('started');
+    startButton.classList.add('hide');
+    shuffleQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
 }
 
 function setNextQuestion() {
@@ -21,4 +25,26 @@ function selectAnswer() {
 
 }
 
-var questions =
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+var questions = [
+    {
+        question: 'what is 2 + 2',
+        answers: [
+            {text: '4', correct: true},
+            {text: '22', correct: false}
+        ]
+    }
+]
