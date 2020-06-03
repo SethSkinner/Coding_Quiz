b43610546f1c68c2a31e97fdeb315e1a9a99cf9a
Seth Skinner <sethwskinner@gmail.com>
2020-06-03 02:42:06 -0500

    added notation to the highscores.html

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,6 +1,6 @@
-
+/*declaring an array for my variable*/
 var usernamesscores = [];
-
+/*function that prevents the default event and pushs submitted info*/
 var addusernamesscores = (e) => {
 	e.preventDefault();
 	var userinfo = {
@@ -8,15 +8,16 @@ var addusernamesscores = (e) => {
 	};
 	usernamesscores.push(userinfo);
 	document.querySelector('form').reset();
-
+/*for display purposes*/
 	console.warn('added', {
 		usernamesscores
 	});
 	var p = document.querySelector('#p');
 	p.textContent = JSON.stringify(usernamesscores);
-
+	/*uses local storage to get the string stored*/
 	localStorage.setItem('userlist', JSON.stringify(usernamesscores));
 };
+/*event listeners on the buttons to run the function when clicked*/
 document.addEventListener('DOMContentLoaded', () => {
 	document.getElementById('saveScoreBtn').addEventListener('click', addusernamesscores);
 });
